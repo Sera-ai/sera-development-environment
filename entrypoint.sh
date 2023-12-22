b7776eec2913d234699ccb98d4a9a7f643bf7504
Brandon [White Fox] <r.rakeda@gmail.com>
2023-12-21 20:26:49 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,22 +12,3 @@ mongod --shutdown
 
 # Start MongoDB in the foreground
 mongod --fork --logpath /var/log/mongod.log --bind_ip_all
-echo "Setting up fe_Builder..."
-npm install --prefix ./fe_Builder && npm --prefix ./fe_Builder run dev &
-
-echo "Setting up fe_Catalog..."
-npm install --prefix ./fe_Catalog && npm --prefix ./fe_Catalog run dev &
-
-echo "Setting up be_Builder..."
-npm install --prefix ./be_Builder && npm --prefix ./be_Builder run dev &
-
-echo "Setting up be_Socket..."
-npm install --prefix ./be_Socket && npm --prefix ./be_Socket run dev &
-
-echo "Setting up be_Router..."
-npm install --prefix ./be_Router && npm --prefix ./be_Router run dev &
-
-echo "Setting up be_Sequencer..."
-npm install --prefix ./be_Sequencer && npm --prefix ./be_Sequencer run dev &
-
-echo "All submodules are set up and running in the background."
