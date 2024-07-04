75a575e73c2d0a480394c03ed5d3f8afb9c2eb6d
NoNameBrandon <r.rakeda@gmail.com>
2024-07-04 01:17:48 -0400

    test2

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 
-# Initialize k3s
-sudo k3s server &
+# Change to the workspace directory
+cd /workspace
 
-# Wait for k3s to be ready
-sleep 30
-
-# Deploy submodule1 to k3s
-kubectl apply -f /workspace/be_Builder/k8s/deployment.yaml
-kubectl apply -f /workspace/be_Builder/k8s/service.yaml
-
-# Deploy submodule1 to k3s
-kubectl apply -f /workspace/be_Sequencer/k8s/deployment.yaml
-kubectl apply -f /workspace/be_Sequencer/k8s/service.yaml
-
-# Deploy submodule1 to k3s
-kubectl apply -f /workspace/be_Socket/k8s/deployment.yaml
-kubectl apply -f /workspace/be_Socket/k8s/service.yaml
-
-# Deploy submodule1 to k3s
-kubectl apply -f /workspace/be_Processor/k8s/deployment.yaml
-kubectl apply -f /workspace/be_Processor/k8s/service.yaml
-
-# Deploy submodule1 to k3s
-kubectl apply -f /workspace/fe_Catalog/k8s/deployment.yaml
-kubectl apply -f /workspace/fe_Catalog/k8s/service.yaml
+# Install shared dependencies (if any)
+# npm install
