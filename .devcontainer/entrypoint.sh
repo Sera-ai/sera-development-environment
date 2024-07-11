c1fe005c79de678f120d3b992d22decbbbb95dbd
brandon <brandonmassie32@gmail.com>
2024-07-10 23:11:56 -0400

    fixing for devenv

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -12,9 +12,5 @@ sed -i 's/\r$//' ./sera-mongodb/entrypoint.sh
 
 # Replace variables in Nginx configuration and start Nginx
 echo "Configuring Nginx..."
-envsubst < ./.devcontainer/nginx.conf > /etc/nginx/nginx.conf
-sed -i 's/___/\$/g' /etc/nginx/nginx.conf
-
-# Start Nginx in foreground
-echo "Starting Nginx..."
-exec nginx -g 'daemon off;'
+envsubst < ./sera-nginx/nginx.conf > /etc/nginx/nginx.conf
+./sera-nginx/entrypoint.sh
