7ce28213977a0c62c0609af9aab445a39ab1798a
brandon <r.rakeda@gmail.com>
2024-08-10 11:24:08 -0400

    bops and boops

--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(dirname "$0")"
-"$SCRIPT_DIR/entrypoint.sh"
 # Function to start a Node.js app
 start_app() {
   local label=$1
@@ -13,11 +11,11 @@ start_app() {
 }
 
 # Start the applications
-start_app "fe_Catalog" "npm run dev" "../sera-frontend"
-start_app "be_Builder" "nodemon" "../sera-backend-core"
-start_app "be_Socket" "nodemon" "../sera-backend-socket"
-start_app "be_Sequencer" "nodemon" "../sera-backend-sequencer"
-start_app "be_Processor" "nodemon" "../sera-backend-processor"
+start_app "fe_Catalog" "npm run dev" "./sera-frontend"
+start_app "be_Builder" "nodemon" "./sera-backend-core"
+start_app "be_Socket" "nodemon" "./sera-backend-socket"
+start_app "be_Sequencer" "nodemon" "./sera-backend-sequencer"
+start_app "be_Processor" "nodemon" "./sera-backend-processor"
 
 # Wait for all background jobs to finish
 wait
